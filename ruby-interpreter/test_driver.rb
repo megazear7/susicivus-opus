42a47a711fdfbe26ed56c74e755c795285a92c63
megazear7 <alexlockhart7@gmail.com>
2014-09-18 11:14:32 -0400

    added a lot of tests

--- ruby-interpreter/test_driver.rb
+++ ruby-interpreter/test_driver.rb
@@ -2,6 +2,9 @@ require "./tokenizer"
 
 test_directory = "./test_cases/"
 
+puts
+full_pass = true
+
 Dir.glob(test_directory + "*.case") do |file_name|
   test_name = file_name[13..-6]
   tokenizer = Tokenizer.new(test_directory + test_name + ".case")
@@ -19,19 +22,32 @@ Dir.glob(test_directory + "*.case") do |file_name|
 
   if should_pass
     if tokenizer.error?
-      puts test_name + ": Error"
+      full_pass = false
+      puts test_name + ":\t Error"
     elsif tokenizer.tokens == answer
-      puts test_name + ": Success" 
+      puts test_name + ":\t Success" 
     else
-      puts test_name + ": Failure"
+      full_pass = false
+      puts test_name + ":\t Failure"
     end
   else
     # some test cases labeled as error cases so that
     # we know that we error when we are supposed to
     if tokenizer.error?
-      puts test_name + ": Success"
+      puts test_name + ":\t Success"
     else
-      puts test_name + ": Failure"
+      full_pass = false
+      puts test_name + ":\t Failure"
     end
   end
 end
+
+puts
+
+if full_pass
+  puts "All tests passed!"
+else
+  puts "Some tests failed or errored"
+end
+
+puts
